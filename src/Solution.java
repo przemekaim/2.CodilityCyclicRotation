1a02186443891717a00b7a11e48d17d03fe09219
Przemek Nosek <przemo99.10@gmail.com>
2021-02-17 20:32:12 +0100

    Lekkie usprawnienie - moim zdaniem bardziej optymalny + kod znaleziony w internecie, calkiem fajny

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,20 @@
-
 public class Solution {
     public int[] solution(int[] A, int K) {
-        if (K == A.length)
-            return A;
-        if (A.length == 0 || A.length == 1)
+        if (A.length == 0 || K == A.length)
             return A;
 
         int rotations = K % A.length;
-
         int[] temp = new int[A.length];
-        for (int i = 0; i < A.length; i++){
-            if (i + rotations == A.length)
-                break;
-            temp[i+rotations] = A[i];
-        }
-        for (int i = 0; i < rotations; i++) {
-            temp[rotations-i-1] = A[A.length-i-1];
-        }
 
+        for (int i = 0; i < A.length - rotations; i++)
+            temp[i + rotations] = A[i];
+
+        for (int i = 0; i < rotations; i++)
+            temp[rotations - i - 1] = A[A.length - i - 1];
+
+        /*for (int i = 0; i < A.length; i++) {
+            temp[(i + K) % A.length] = A[i];
+        }*/
         return temp;
     }
 }
